38782ba44b32aa1d25a24744b12821759c7f1d2a
christophertorres0 <31553679+christophertorres0@users.noreply.github.com>
2019-01-28 14:42:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function removeColor(el){
 }
 
 function showTrophy(el){
-		document.getElemenetByID("opacity-crams").src="images/"lombardi_trophy.png";
-		document.getElemenetByID("mid-column").src="images/"lombardi_trophy.png";
-		document.getElemenetByID("opacity-pats).src="images/"lombardi_trophy.png";
+		document.getElemenetByID("opacity-crams").src="images/lombardi_trophy.png";
+		document.getElemenetByID("mid-column").src="images/lombardi_trophy.png";
+		document.getElemenetByID("opacity-pats").src="images/lombardi_trophy.png";
 		
 		
 		
